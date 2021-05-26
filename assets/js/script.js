2123ff2549acde5cc206c3d6343cdd17a52f8c06
harmonyh <harmony.h@gmail.com>
2021-05-26 11:50:55 -0400

    first commit/basic HTML structure

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/* 
+6.2.4 to handle form submission 
+
+submit form, get value from input element (nameinputEl) and store it into its own variable (username)
+.trim() to get rid of trailing or leading spaces
+check value in username variable - pass it to getUserRepos
+
+uses form to search github repos, next will display
+
+*/
+
+/* 
+6.2.5 displaying repo
+
+for loop - take each repo (repos[i]) and write some data to page - format appearance of name and repo name
+div element
+span to hold formatted repo name
+add that to the div, and add entire div to container created earlier
+
+*/
